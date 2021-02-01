98af862b49341f6f350563b0c19e41319a50e21a
wuyaheng <wuyaheng2016@gmail.com>
2021-02-01 15:33:49 -0500

    add ag-grid

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,26 +1,43 @@
-import React from "react";
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function Table(props) {
+const Table = (props) => {
+    const [gridApi, setGridApi] = useState(null);
+    const [gridColumnApi, setGridColumnApi] = useState(null);
+
+    
+
+    function onGridReady(params) {
+        setGridApi(params.api);
+        setGridColumnApi(params.columnApi);
+        params.api.sizeColumnsToFit();
+    }
   
-  return (
-        <div className="card">
-        <table class="table table-hover">
-        <thead>
-            <tr>
-            <th scope="col" width="30%">Volunteer Opportunity</th>
-            <th scope="col" width="30%">Website</th>
-            <th scope="col" width="40%">Requirements</th> 
-            </tr>
-        </thead>
-        <tbody>
-            {props.results.map((ele, i) => <tr><td>{ele.opportunity_id}</td><td><a href={ele.website} rel='noopener noreferrer' target='_blank'>{ele.website}</a></td><td> {ReactHtmlParser(ele.requirements)}</td></tr>)} 
-        </tbody>
-        </table>
+    let rowData = []
+    props.results.map((ele, i) => rowData.push( 
+      {Volunteer: ele.opportunity_id, 
+      Website: ele.website, 
+      Requirements: ele.requirements}
+      ))
 
+
+    return (
+        <div className="ag-theme-alpine" style={ { height: 400, width: 1550 } }>
+            <AgGridReact
+                onGridReady={onGridReady}
+                rowData={rowData}>
+                <AgGridColumn field="Volunteer"></AgGridColumn>
+                <AgGridColumn field="Website"></AgGridColumn>
+                <AgGridColumn field="Requirements"></AgGridColumn>
+            </AgGridReact>
         </div>
-  );
-}
+    );
+};
 
 export default Table;
