f6f875f8d0d81d100bc4ac54c348e8f774d1f1af
wuyaheng <wuyaheng2016@gmail.com>
2021-02-01 13:05:32 -0500

    add all opportunites selection

--- src/components/OpportunitiesSelect/index.js
+++ src/components/OpportunitiesSelect/index.js
@@ -5,6 +5,7 @@ function OpportunitiesSelect({ results, handleOppChange }) {
     return (
         <div className="input-group mb-2">
             <select className="custom-select" id="inputGroupSelect02" onChange={handleOppChange}>
+                <option value="All Volunteer Opportunities" selected="selected">All Volunteer Opportunities</option>
                 {results.map((ele, i) => (
                     ele.opportunity_id ? <option key={i + "-el"} value={ele.opportunity_id}>{ele.opportunity_id}</option> : null
                 ))}
